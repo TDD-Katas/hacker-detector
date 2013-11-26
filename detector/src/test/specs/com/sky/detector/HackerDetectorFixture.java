0435bb2e66f07d42db85fd3e6db0a30f47f4f889
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-11-26 20:05:37 +0200

    Added the acceptance test

--- detector/src/test/specs/com/sky/detector/HackerDetectorFixture.java
+++ detector/src/test/specs/com/sky/detector/HackerDetectorFixture.java
@@ -4,7 +4,7 @@ import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
-public class HackerDetector {
+public class HackerDetectorFixture {
 
 	public String parseLine(String logLine) {
 		return "Hello world !";
