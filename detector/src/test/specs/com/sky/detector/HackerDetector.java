fa620a0cfd4d79d7abf1d95806e03eee91056cd3
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-11-26 20:02:17 +0200

    Renamed the main class

--- detector/src/test/specs/com/sky/detector/HackerDetector.java
+++ detector/src/test/specs/com/sky/detector/HackerDetector.java
@@ -4,7 +4,7 @@ import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(ConcordionRunner.class)
-public class AppFixture {
+public class HackerDetector {
 
 	public String parseLine(String logLine) {
 		return "Hello world !";
