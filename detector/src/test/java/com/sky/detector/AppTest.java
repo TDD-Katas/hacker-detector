9fd2916723f473e5fab4e857811c10c516cb83aa
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-11-26 20:10:33 +0200

    First test draft

--- detector/src/test/java/com/sky/detector/AppTest.java
+++ detector/src/test/java/com/sky/detector/AppTest.java
@@ -7,15 +7,19 @@ package com.sky.detector;
 import org.junit.Test;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.mock;
 
 /**
  *
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class AppTest {
+    
     @Test
-    public void testMain() {
-        assertThat(true, is(true));
+    public void should_return_ip_for_if_line_is_offending() {
+        String ip = "ip";
+        
+        String result = ip;
+        
+        assertThat(result, is(ip));
     }
 }
