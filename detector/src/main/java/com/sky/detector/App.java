051411baab4c22e1ac4d14512097d99f1d3fe63d
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-11-26 19:56:36 +0200

    Project setup, copied the project description to concordion

---
+++ detector/src/main/java/com/sky/detector/App.java
@@ -0,0 +1,13 @@
+package com.sky.detector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
